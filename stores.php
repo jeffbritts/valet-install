3f1420239d9d57dd946a66e32019559dd6a96829
Jeff Britts <jbritts@adobe.com>
2023-04-11 13:49:37 -0500

    updates

--- stores.php
+++ stores.php
@@ -3,7 +3,7 @@
 use \Magento\Store\Model\StoreManager;
 
 $serverName = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : null;
-$allowedSiteCodes = ['fresh','auto','b2b','b2c','sitea','siteb','sitec','sited','sitee','luma','venia','brentmill','healthbeauty','wknd','wecafe','bodea'];
+$allowedSiteCodes = ['fresh','auto','b2b','b2c','sitea','siteb','sitec','sited','sitee','luma','venia','brentmill','healthbeauty','wknd','wecafe','bodea','frescopa'];
 $runCode = '';
 $runType = 'website';
 if($serverName){
